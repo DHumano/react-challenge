50099ba34eb7462f1903b34627b833bfe5ca4882
Dario Humano <dario.humano@QUALITEST.LOCAL>
2022-07-05 16:59:46 -0300

    fixes

--- src/components/filters/Styled.jsx
+++ src/components/filters/Styled.jsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
-const Text = styled.p``;
+const Text = styled.div``;
 
 export { Wrapper, Text };
